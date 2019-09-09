b1994117777e1a6fe4b0300cd303362ddac17bd3
Nathannael <nateshade7@gmail.com>
2019-09-09 05:30:39 -0700

    add billboard component to spider in game.ts

--- src/game.ts
+++ src/game.ts
@@ -2,3 +2,7 @@
 //DCL Code setup by Red Magnus
 const spider = new entity()
 spider.addComponent(new GLTFShape("models/spider1.glb"))
+spider.addComponent(new Transform({
+  position: new Vector3(5, 1, 5)
+}))
+engine.addEntity(spider)
