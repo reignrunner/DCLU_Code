360d23ed2755d1a0aedcac0160cf8057627bde41
Nathannael <nateshade7@gmail.com>
2019-09-09 06:49:44 -0700

    Create Spider Spawner and Remove Billboard

--- src/game.ts
+++ src/game.ts
@@ -1,15 +1,26 @@
-//spider plane
+//spider spawner
 //DCL Code setup by Red Magnus
+//the spawn code
+function spawnSpider(x: number, y: number, z: number) {
+  //make the spider entity & define the spider's model
+  let spider = new Entity()
+  let spiderShape = new GLTFShape("models/spider1.glb")
+  //define spider's position, size and direction
+  spider.addComponent(spiderShape)
+  spider.addComponent(new Transform({
+    position: new Vector3(x, y, z),
+    scale: new Vector3(0.25, 0.25, 0.25)
+  }))
+//  spider.addComponent(new Billboard())
+  //show the spider on screen
+  engine.addEntity(spider)
 
-//make the spider
-const spider = new entity()
-//define the spider's model
-spider.addComponent(new GLTFShape("models/spider1.glb"))
-//define spider's position, size and direction
-spider.addComponent(new Transform({
-  position: new Vector3(8, 1, 8),
-  scale: new Vector3(2, 2, 2)
-}))
-spider.addComponent(new Billboard())
-//show the spider on screen
-engine.addEntity(spider)
+  return spider
+}
+
+function spiderSpawning(howmany: number) {
+  for (let i = 0; i < howmany; i++) {
+    spawnSpider(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
+  }
+}
+spiderSpawning(3)
