9dafed46a59296da75422da5a0ed02486537492e
Nathannael <nateshade7@gmail.com>
2019-09-09 05:47:44 -0700

    no but really this time

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,15 @@
 //spider plane
 //DCL Code setup by Red Magnus
+
+//make the spider
 const spider = new entity()
+//define the spider's model
 spider.addComponent(new GLTFShape("models/spider1.glb"))
+//define spider's position, size and direction
 spider.addComponent(new Transform({
-  position: new Vector3(5, 1, 5)
+  position: new Vector3(8, 1, 8),
+  scale: new Vector3(2, 2, 2)
 }))
+spider.addComponent(new Billboard())
+//show the spider on screen
 engine.addEntity(spider)
