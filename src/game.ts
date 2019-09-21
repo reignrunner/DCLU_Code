2dba82d5d78025a11a02933998f44c612a1deb1d
Nathannael <nateshade7@gmail.com>
2019-09-20 22:01:14 -0700

    Auto stash before merge of "master" and "origin/master"

--- src/game.ts
+++ src/game.ts
@@ -1,16 +1,31 @@
 //spider spawner
+import {BuilderHUD} from '/src/modules/BuilderHUD.ts'
 //DCL Code setup by Red Magnus
 //the spawn code
+<<<<<<< Updated upstream
 //make the spider entity & define the spider's model
 let spider = new Entity()
 let spiderShape = new GLTFShape("models/spider1.glb")
 function spawnSpider(x: number, y: number, z: number) {
   //define the individual spider's position & size
+=======
+const hud:BuilderHUD =  new BuilderHUD()
+hud.setDefaultParent(scene)
+function spawnSpider(x: number, y: number, z: number) {
+  //make the spider entity & define the spider's model
+  let spider = new Entity()
+  let spiderShape = new GLTFShape("models/spider1.glb")
+  //define spider's position and size
+>>>>>>> Stashed changes
   spider.addComponent(spiderShape)
   spider.addComponent(new Transform({
     position: new Vector3(x, y, z),
     scale: new Vector3(0.25, 0.25, 0.25)
   }))
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
   //show the spider on screen
   engine.addEntity(spider)
 
@@ -22,4 +37,6 @@ function spiderSpawning(howmany: number) {
     spawnSpider(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
   }
 }
-spiderSpawning(3)
+spiderSpawning(0)
+spawnSpider(8, 8, 8)
+hud.attachToEntity(spider)
